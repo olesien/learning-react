39a7d98d9d294cd1d0896c5cfd30ba7ec5e09477
olesien <olesien2@gmail.com>
2022-04-19 14:13:24 +0200

    added redirect to home page

--- 10-better-todos/src/components/AddNewTodoForm.jsx
+++ 10-better-todos/src/components/AddNewTodoForm.jsx
@@ -1,49 +1,52 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const AddNewTodoForm = ({ onAddNewTodo }) => {
-	// input state
-	const [newTodoTitle, setNewTodoTitle] = useState('')
-	// input reference
-	const newTodoTitleRef = useRef()
+    const navigate = useNavigate();
+    // input state
+    const [newTodoTitle, setNewTodoTitle] = useState("");
+    // input reference
+    const newTodoTitleRef = useRef();
 
-	const handleSubmit = e => {
-		// stop form from submitting
-		e.preventDefault()
+    const handleSubmit = (e) => {
+        // stop form from submitting
+        e.preventDefault();
 
-		// push a new todo to the todos state
-		const newTodo = { title: newTodoTitle, completed: false }
-		onAddNewTodo(newTodo)
+        // push a new todo to the todos state
+        const newTodo = { title: newTodoTitle, completed: false };
+        onAddNewTodo(newTodo);
 
-		// clear newTodoTitle state
-		setNewTodoTitle('')
-	}
+        // clear newTodoTitle state
+        setNewTodoTitle("");
+        navigate("/", { replace: true });
+    };
 
-	// focus on input field when component is mounted
-	useEffect(() => {
-		newTodoTitleRef.current.focus()
-	}, [])
+    // focus on input field when component is mounted
+    useEffect(() => {
+        newTodoTitleRef.current.focus();
+    }, []);
 
-	return (
-		<form onSubmit={handleSubmit}>
-			<div className="input-group">
-				<input
-					type="text"
-					className="form-control"
-					placeholder="Todo title"
-					ref={newTodoTitleRef}
-					onChange={e => setNewTodoTitle(e.target.value)}
-					value={newTodoTitle}
-				/>
-				<button
-					type="submit"
-					className="btn btn-primary"
-					disabled={!newTodoTitle.length}
-				>
-					Create
-				</button>
-			</div>
-		</form>
-	)
-}
+    return (
+        <form onSubmit={handleSubmit}>
+            <div className="input-group">
+                <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Todo title"
+                    ref={newTodoTitleRef}
+                    onChange={(e) => setNewTodoTitle(e.target.value)}
+                    value={newTodoTitle}
+                />
+                <button
+                    type="submit"
+                    className="btn btn-primary"
+                    disabled={!newTodoTitle.length}
+                >
+                    Create
+                </button>
+            </div>
+        </form>
+    );
+};
 
-export default AddNewTodoForm
+export default AddNewTodoForm;
