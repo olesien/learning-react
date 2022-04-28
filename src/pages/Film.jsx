ba70c4c07f73e4a0176dac258bc4a39a6cdd4ee9
olesien <olesien2@gmail.com>
2022-04-28 12:58:27 +0200

    Added back button

--- src/pages/Film.jsx
+++ src/pages/Film.jsx
@@ -1,16 +1,18 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
 import Table from "react-bootstrap/Table";
 
 import SwapiAPI from "../services/SwapiAPI";
-import { Spinner } from "react-bootstrap";
+import { Button, Spinner } from "react-bootstrap";
 import RenderList from "../components/RenderList";
 
 export default function Film() {
     const [film, setFilm] = useState();
     const { id } = useParams();
+
+    const navigate = useNavigate();
 
     //Get the film
     useEffect(() => {
@@ -23,49 +25,58 @@ export default function Film() {
     return (
         <Container style={{ marginTop: "10px" }}>
             {film ? (
-                <Card>
-                    <Card.Header>{film.title}</Card.Header>
-                    <Card.Body>
-                        <h3>Attributes</h3>
-                        <Table>
-                            <tbody>
-                                <tr>
-                                    <th>Episode</th>
-                                    <td>{film.episode_id}</td>
-                                </tr>
-                                <tr>
-                                    <th>Director</th>
-                                    <td>{film.director}</td>
-                                </tr>
-                                <tr>
-                                    <th>Producer</th>
-                                    <td>{film.producer}</td>
-                                </tr>
-                                <tr>
-                                    <th>Release Date</th>
-                                    <td>{film.release_date}</td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <h3 style={{ marginTop: "2rem" }}>
-                                            Links
-                                        </h3>
-                                    </td>
-                                </tr>
+                <>
+                    <Card>
+                        <Card.Header>{film.title}</Card.Header>
+                        <Card.Body>
+                            <h3>Attributes</h3>
+                            <Table>
+                                <tbody>
+                                    <tr>
+                                        <th>Episode</th>
+                                        <td>{film.episode_id}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Director</th>
+                                        <td>{film.director}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Producer</th>
+                                        <td>{film.producer}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Release Date</th>
+                                        <td>{film.release_date}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <h3 style={{ marginTop: "2rem" }}>
+                                                Links
+                                            </h3>
+                                        </td>
+                                    </tr>
 
-                                <tr>
-                                    <th>Characters</th>
-                                    <td>
-                                        <RenderList
-                                            caption="Character"
-                                            array={film.characters}
-                                        />
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </Table>
-                    </Card.Body>
-                </Card>
+                                    <tr>
+                                        <th>Characters</th>
+                                        <td>
+                                            <RenderList
+                                                caption="Character"
+                                                array={film.characters}
+                                            />
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </Table>
+                        </Card.Body>
+                    </Card>
+                    <Button
+                        style={{ margin: "10px" }}
+                        variant="primary"
+                        onClick={() => navigate(-1)}
+                    >
+                        Go back
+                    </Button>
+                </>
             ) : (
                 <Spinner animation="border" role="status">
                     <span className="visually-hidden">Loading...</span>
--- src/pages/People.jsx
+++ src/pages/People.jsx
@@ -1,16 +1,18 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
 import Table from "react-bootstrap/Table";
 
 import SwapiAPI from "../services/SwapiAPI";
-import { Spinner } from "react-bootstrap";
+import { Button, Spinner } from "react-bootstrap";
 import RenderList from "../components/RenderList";
 
 export default function People() {
     const [people, setPeople] = useState();
     const { id } = useParams();
+
+    const navigate = useNavigate();
 
     //Get the person
     useEffect(() => {
@@ -23,61 +25,70 @@ export default function People() {
     return (
         <Container style={{ marginTop: "10px" }}>
             {people ? (
-                <Card>
-                    <Card.Header>{people.name}</Card.Header>
-                    <Card.Body>
-                        <h3>Attributes</h3>
-                        <Table>
-                            <tbody>
-                                <tr>
-                                    <th>Gender</th>
-                                    <td>{people.gender}</td>
-                                </tr>
-                                <tr>
-                                    <th>Birth year</th>
-                                    <td>{people.birth_year}</td>
-                                </tr>
-                                <tr>
-                                    <th>Height</th>
-                                    <td>{people.height}</td>
-                                </tr>
-                                <tr>
-                                    <th>Mass</th>
-                                    <td>{people.mass}</td>
-                                </tr>
-                                <tr>
-                                    <th>Hair color</th>
-                                    <td>{people.hair_color}</td>
-                                </tr>
-                                <tr>
-                                    <th>Skin color</th>
-                                    <td>{people.skin_color}</td>
-                                </tr>
-                                <tr>
-                                    <th>Eye color</th>
-                                    <td>{people.eye_color}</td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <h3 style={{ marginTop: "2rem" }}>
-                                            Links
-                                        </h3>
-                                    </td>
-                                </tr>
+                <>
+                    <Card>
+                        <Card.Header>{people.name}</Card.Header>
+                        <Card.Body>
+                            <h3>Attributes</h3>
+                            <Table>
+                                <tbody>
+                                    <tr>
+                                        <th>Gender</th>
+                                        <td>{people.gender}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Birth year</th>
+                                        <td>{people.birth_year}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Height</th>
+                                        <td>{people.height}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Mass</th>
+                                        <td>{people.mass}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Hair color</th>
+                                        <td>{people.hair_color}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Skin color</th>
+                                        <td>{people.skin_color}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Eye color</th>
+                                        <td>{people.eye_color}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <h3 style={{ marginTop: "2rem" }}>
+                                                Links
+                                            </h3>
+                                        </td>
+                                    </tr>
 
-                                <tr>
-                                    <th>Characters</th>
-                                    <td>
-                                        <RenderList
-                                            caption="Film"
-                                            array={people.films}
-                                        />
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </Table>
-                    </Card.Body>
-                </Card>
+                                    <tr>
+                                        <th>Characters</th>
+                                        <td>
+                                            <RenderList
+                                                caption="Film"
+                                                array={people.films}
+                                            />
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </Table>
+                        </Card.Body>
+                    </Card>
+                    <Button
+                        style={{ margin: "10px" }}
+                        variant="primary"
+                        onClick={() => navigate(-1)}
+                    >
+                        Go back
+                    </Button>
+                </>
             ) : (
                 <Spinner animation="border" role="status">
                     <span className="visually-hidden">Loading...</span>
