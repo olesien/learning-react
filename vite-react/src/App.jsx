3b7b3becd94e5e11834c670bd6104b697914aebc
olesien <olesien2@gmail.com>
2022-06-08 10:25:45 +0200

    Created basic vite app

---
+++ vite-react/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+// import "./App.scss";
+
+function App() {
+    return (
+        <div className="App">
+            <p>hi</p>
+        </div>
+    );
+}
+
+export default App;
