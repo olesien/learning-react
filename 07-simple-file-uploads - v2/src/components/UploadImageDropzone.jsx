2fe24a21063404b509be37520a0e3feed4199fc6
olesien <olesien2@gmail.com>
2022-09-14 10:39:45 +0200

    Added foundation for memehub

--- 07-simple-file-uploads - v2/src/components/UploadImageDropzone.jsx
+++ 07-simple-file-uploads - v2/src/components/UploadImageDropzone.jsx
@@ -1,63 +1,93 @@
-import { useCallback } from 'react'
-import { useDropzone } from 'react-dropzone'
-import Alert from 'react-bootstrap/Alert'
-import ProgressBar from 'react-bootstrap/ProgressBar'
-import classNames from 'classnames'
-import useUploadImage from '../hooks/useUploadImage'
+import { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+import Alert from "react-bootstrap/Alert";
+import ProgressBar from "react-bootstrap/ProgressBar";
+import classNames from "classnames";
+import useUploadImage from "../hooks/useUploadImage";
 
 const UploadImageDropzone = () => {
-	const uploadImage = useUploadImage()
+    const uploadImage = useUploadImage();
 
-	const onDrop = useCallback((acceptedFiles) => {
-		console.log("Got me zum files 😋", acceptedFiles)
+    const onDrop = useCallback((acceptedFiles) => {
+        console.log("Got me zum files 😋", acceptedFiles);
 
-		if (!acceptedFiles.length) {
-			return
-		}
+        if (!acceptedFiles.length) {
+            return;
+        }
 
-		uploadImage.mutate(acceptedFiles[0])
-	}, [])
+        uploadImage.mutate(acceptedFiles[0]);
+    }, []);
 
-	const { getRootProps, getInputProps, isDragActive, isDragAccept, isDragReject } = useDropzone({
-		accept: {
-			'image/gif': ['.gif'],
-			'image/jpeg': ['.jpg', '.jpeg'],
-			'image/png': ['.png'],
-			'image/webp': ['.webp'],
-		},
-		onDrop,
-	})
+    const {
+        acceptedFiles,
+        getRootProps,
+        getInputProps,
+        isDragActive,
+        isDragAccept,
+        isDragReject,
+    } = useDropzone({
+        accept: {
+            "image/gif": [".gif"],
+            "image/jpeg": [".jpg", ".jpeg"],
+            "image/png": [".png"],
+            "image/webp": [".webp"],
+        },
+        mazSize: 2 * 1024 * 1024,
+        onDrop,
+    });
 
-	const cssClasses = classNames({
-		'drag-accept': isDragAccept,
-		'drag-reject': isDragReject,
-	})
+    const cssClasses = classNames({
+        "drag-accept": isDragAccept,
+        "drag-reject": isDragReject,
+    });
 
-	return (
-		<div {...getRootProps()} id="upload-image-dropzone-wrapper" className={cssClasses}>
-			<input {...getInputProps()} />
+    return (
+        <div
+            {...getRootProps()}
+            id="upload-image-dropzone-wrapper"
+            className={cssClasses}
+        >
+            <input {...getInputProps()} />
 
-			{
-				isDragActive
-					? isDragAccept
-						? <p>Drop it like its hot 🔥!</p>
-						: <p>We don't want that file 😡!</p>
-					: <p>Give me some files 😋!</p>
-			}
+            {isDragActive ? (
+                isDragAccept ? (
+                    <p>Drop it like its hot 🔥!</p>
+                ) : (
+                    <p>We don't want that file 😡!</p>
+                )
+            ) : (
+                <p>Give me some files 😋!</p>
+            )}
 
-			{uploadImage.isMutating && (
-				<ProgressBar
-					animated
-					now={uploadImage.uploadProgress}
-					label={`${uploadImage.uploadProgress}%`}
-					variant="success"
-				/>
-			)}
+            {uploadImage.isMutating && (
+                <ProgressBar
+                    animated
+                    now={uploadImage.uploadProgress}
+                    label={`${uploadImage.uploadProgress}%`}
+                    variant="success"
+                />
+            )}
 
-			{uploadImage.isError && <Alert variant="warning">{uploadImage.error.message}</Alert>}
-			{uploadImage.isSuccess && <Alert variant="success">File uploaded successfully ✨!</Alert>}
-		</div>
-	)
-}
+            {acceptedFiles.length > 0 && (
+                <div className="accepted-fioles mt-2">
+                    <ul className="list-unstyled">
+                        {acceptedFiles.map((file, index) => (
+                            <li key={index}>
+                                {file.name} {Math.round(file.size)}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
 
-export default UploadImageDropzone
+            {uploadImage.isError && (
+                <Alert variant="warning">{uploadImage.error.message}</Alert>
+            )}
+            {uploadImage.isSuccess && (
+                <Alert variant="success">File uploaded successfully ✨!</Alert>
+            )}
+        </div>
+    );
+};
+
+export default UploadImageDropzone;
