6716d1d8936187607d42b301f7e068796ea149fa
olesien <olesien2@gmail.com>
2022-09-20 15:41:20 +0200

    added describe

--- 10-unit-testing/src/components/LoginForm.test.jsx
+++ 10-unit-testing/src/components/LoginForm.test.jsx
@@ -1,41 +1,43 @@
 import { render, screen } from "@testing-library/react";
 import LoginForm from "./LoginForm";
 
-it("does not show loading spinner", () => {
-    // Render the LoginForm component
-    render(<LoginForm />);
+describe(":(", () => {
+    it("does not show loading spinner", () => {
+        // Render the LoginForm component
+        render(<LoginForm />);
 
-    //     //Find the elements we want to interact with
-    const loadingElement = screen.queryByText(/Loading.../i);
-    //     //Interact with said elements
+        //     //Find the elements we want to interact with
+        const loadingElement = screen.queryByText(/Loading.../i);
+        //     //Interact with said elements
 
-    //     //Assert that the results are what we expect them to be
-    expect(loadingElement).not.toBeInTheDocument();
-});
+        //     //Assert that the results are what we expect them to be
+        expect(loadingElement).not.toBeInTheDocument();
+    });
 
-it("renders input fields", () => {
-    // Render the LoginForm component
-    render(<LoginForm />);
-    const emailFields = screen.getAllByRole("textbox", { name: /Email/i });
-    expect(emailFields).toHaveLength(2);
+    it("renders input fields", () => {
+        // Render the LoginForm component
+        render(<LoginForm />);
+        const emailFields = screen.getAllByRole("textbox", { name: /Email/i });
+        expect(emailFields).toHaveLength(2);
 
-    const passwordFields = screen.getAllByText(/Password/);
-    expect(passwordFields).toHaveLength(2);
-});
+        const passwordFields = screen.getAllByText(/Password/);
+        expect(passwordFields).toHaveLength(2);
+    });
 
-it("renders input fields initially empty", () => {
-    // Render the LoginForm component
-    render(<LoginForm />);
+    it("renders input fields initially empty", () => {
+        // Render the LoginForm component
+        render(<LoginForm />);
 
-    const loadingElement = screen.queryByText(/Loading.../i);
+        const loadingElement = screen.queryByText(/Loading.../i);
 
-    if (loadingElement) {
-        const emailFields = screen.queryAllByRole("textbox", {
-            name: /Email/i,
-        });
-        expect(emailFields).toHaveLength(0);
+        if (loadingElement) {
+            const emailFields = screen.queryAllByRole("textbox", {
+                name: /Email/i,
+            });
+            expect(emailFields).toHaveLength(0);
 
-        const passwordFields = screen.queryAllByText(/Password/);
-        expect(passwordFields).toHaveLength(0);
-    }
+            const passwordFields = screen.queryAllByText(/Password/);
+            expect(passwordFields).toHaveLength(0);
+        }
+    });
 });
